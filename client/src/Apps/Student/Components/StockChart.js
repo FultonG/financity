c31f2db939c12dc23cc9762a5cf61f169ad07556
Lazaro Hurtado <hurtadolazaro12@gmail.com>
2020-10-04 13:21:40 -0400

    chart formatting

--- client/src/Apps/Student/Components/StockChart.js
+++ client/src/Apps/Student/Components/StockChart.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line} from "recharts";
+import React from "react";
+import {
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line,
+} from "recharts";
 
-const StockChart =  ({ width=275, height=150, data }) => {
-    return (
-        <LineChart width={width} height={height} data={data} 
-            margin={{ top: 25, right: 30, left: 0, bottom: 25 }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="time" axisLine={false} tickLine={false} hide={true} mirror/>
-            <YAxis dataKey="value" domain={['dataMin - 10', 'dataMax + 10']} />
-            <Tooltip />
-            {/* <Legend /> */}
-            {/* <Line type="monotone" dataKey="pv" stroke="#8884d8" />   */}
-            <Line type="monotone" dataKey="value" stroke="#17ce17" strokeWidth={3} dot={false}/>
-        </LineChart>
-    )
-}
+const StockChart = ({ width = 475, height = 150, data }) => {
+  return (
+    <LineChart
+      width={width}
+      height={height}
+      data={data}
+      margin={{ top: 25, right: 30, left: 10, bottom: 25 }}
+    >
+      <CartesianGrid strokeDasharray="10 10" />
+      <XAxis
+        dataKey="time"
+        axisLine={false}
+        tickLine={true}
+        hide={true}
+        mirror
+      />
+      <YAxis
+        tickFormatter={(t) => t.toFixed(2)}
+        allowDecimals={false}
+        dataKey="value"
+        type="number"
+        domain={["dataMin - 20", "dataMax + 20"]}
+      />
+      <Tooltip />
+      {/* <Legend /> */}
+      {/* <Line type="monotone" dataKey="pv" stroke="#8884d8" />   */}
+      <Line
+        type="monotone"
+        dataKey="value"
+        stroke="#17ce17"
+        strokeWidth={3}
+        dot={false}
+      />
+    </LineChart>
+  );
+};
 
-export default StockChart
+export default StockChart;
--- client/src/Apps/Student/Stocks.js
+++ client/src/Apps/Student/Stocks.js
@@ -1,77 +1,107 @@
-import React, { useEffect, useState } from 'react';
-import Container from '../../Components/Container';
-import Card from '../../Components/Card';
-import { Text, Title, Description } from '../../Components/Text';
-import API from './utils/API';
-import Button from '../../Components/Button';
-import Check from '../../images/check.gif'
-import Modal from '../../Components/Modal';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import Container from "../../Components/Container";
+import Card from "../../Components/Card";
+import { Text, Title, Description } from "../../Components/Text";
+import API from "./utils/API";
+import Button from "../../Components/Button";
+import Check from "../../images/check.gif";
+import Modal from "../../Components/Modal";
+import { useHistory } from "react-router-dom";
 import moment from "moment";
-import StockChart from './Components/StockChart';
+import StockChart from "./Components/StockChart";
 
 const Stocks = ({ user }) => {
-  const [stocks, setStocks] = useState([])
+  const [stocks, setStocks] = useState([]);
   const [success, setSuccess] = useState(false);
   let history = useHistory();
 
-  var formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 0
+  var formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
   });
 
   useEffect(() => {
     const fetchStocks = async () => {
-        let res = await API.getStocks(user.classroom_id).then(res => res.data);
-        //   setStocks(res.data);
+      let res = await API.getStocks(user.classroom_id).then((res) => res.data);
+      //   setStocks(res.data);
 
-        let newStocks = [];
-        for(const key in res) {
-            let stockData = res[key];
-            let now = moment().format('YYYY-MM-DD');
-            let before = moment().subtract(1, 'month').format('YYYY-MM-DD');
-            
-            let { data } = await API.getStockHistoricalData(stockData.symbol, before, now);
-            stockData.historical = data.map(datum => {
-              return {...datum, close: Math.floor(datum.close)}
-            }).reverse();
-            newStocks.push(stockData);
-        }
-        setStocks(newStocks);
-    }
+      let newStocks = [];
+      for (const key in res) {
+        let stockData = res[key];
+        let now = moment().format("YYYY-MM-DD");
+        let before = moment().subtract(1, "year").format("YYYY-MM-DD");
+
+        let { data } = await API.getStockHistoricalData(
+          stockData.symbol,
+          before,
+          now
+        );
+        stockData.historical = data
+          .map((datum) => {
+            return { ...datum, close: parseFloat(datum.close).toFixed(2) };
+          })
+          .reverse();
+        newStocks.push(stockData);
+      }
+      setStocks(newStocks);
+    };
 
     fetchStocks();
-  }, [])
+  }, []);
 
   const handlePurchase = async (ticker, shares) => {
     try {
-      let res = await API.buyStock({username: user.username, ticker, shares});
+      let res = await API.buyStock({ username: user.username, ticker, shares });
       console.log(res.data);
       setSuccess(true);
     } catch (e) {
       console.log(e.message);
     }
-  }
+  };
   function returnStocks() {
     let cards = [];
-    for(const key in stocks) {
-        let stock = stocks[key];
-        let historicalData = stock.historical.map(h => { return { time: h.date, value: h.close} });
-        cards.push(
-          <Card justify="flex-start" margin="0px 0px 10px 0px" hover cursor="pointer">
+    for (const key in stocks) {
+      let stock = stocks[key];
+      let historicalData = stock.historical.map((h) => {
+        const date = new Date(h.date);
+        const month = date.getUTCMonth();
+        const day = date.getUTCDay();
+        const year = date.getUTCFullYear();
+        const dateFormat = month + "-" + day + "-" + year;
+
+        return { time: dateFormat, value: parseFloat(h.close) };
+      });
+      console.log(historicalData);
+      cards.push(
+        <Card
+          justify="flex-start"
+          margin="0px 0px 10px 0px"
+          hover
+          cursor="pointer"
+        >
           {/* <img style={{ width: '20%', marginRight: '20px' }} src={property.thumbnail}></img> */}
           {/* <div style={{ width: '20%', marginRight: '20px' }}>graph</div> */}
-          <StockChart data={historicalData} style={{padding: '15px'}}/>
-          <Container width="50%" direction="column" align="self-start" padding="20px" justify="space-between">
-              {/* <Description>Beds: {property.beds} Baths: {property.baths} </Description>
+          <StockChart data={historicalData} style={{ padding: "15px" }} />
+          <Container
+            width="50%"
+            direction="column"
+            align="self-start"
+            padding="20px"
+            justify="space-between"
+          >
+            {/* <Description>Beds: {property.beds} Baths: {property.baths} </Description>
               <Description>{property.address.line} {property.address.city} {property.address.postal_code}</Description>
               <Text>Price: {formatter.format(property.price)}</Text> */}
-              <h3>{stock.symbol} - {stock.name}</h3>
-              <Button onClick={() => handlePurchase(stock.symbol)}>Buy now!</Button>
+            <h3>
+              {stock.symbol} - {stock.name}
+            </h3>
+            <Button onClick={() => handlePurchase(stock.symbol)}>
+              Buy now!
+            </Button>
           </Container>
-          </Card>
-        )
+        </Card>
+      );
     }
     return cards;
   }
@@ -84,14 +114,19 @@ const Stocks = ({ user }) => {
         </Container>
       </Container>
       <Modal show={success}>
-        <Card height="60%" width="50%" direction="column" justify="space-evenly">
+        <Card
+          height="60%"
+          width="50%"
+          direction="column"
+          justify="space-evenly"
+        >
           <img src={Check} />
           <Text>Home Purchased!</Text>
-          <Button onClick={() => history.push('/')}>Go to Dashboard</Button>
+          <Button onClick={() => history.push("/")}>Go to Dashboard</Button>
         </Card>
       </Modal>
     </>
-  )
+  );
 };
 
 export default Stocks;
